0897141b4cdab2bd2fcd577e5b2f3d4c798c7a8a
GlebsIT <belgberez@gmail.com>
2020-04-10 00:11:25 +0400

    Updates

---
+++ precache-manifest.5c698e1389aec0c76361d0ecf17ba9a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ea581be19e0576ac43a68ae6ec5d6d",
+    "url": "/whois/index.html"
+  },
+  {
+    "revision": "3691ec3cccc6c835a37f",
+    "url": "/whois/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "131efcef10b40518c7e4",
+    "url": "/whois/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3691ec3cccc6c835a37f",
+    "url": "/whois/static/js/2.269bbd7b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/whois/static/js/2.269bbd7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131efcef10b40518c7e4",
+    "url": "/whois/static/js/main.4a546159.chunk.js"
+  },
+  {
+    "revision": "4f479994a0aabc076440",
+    "url": "/whois/static/js/runtime-main.bcbe7b29.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/whois/static/media/persik.4e1ec840.png"
+  }
+]);
